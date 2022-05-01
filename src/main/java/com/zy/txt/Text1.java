5ee388b641acd84d9374b38dbc01f319afdc3dcc
这是哪头猪? <koi-pig@qq.com>
2022-05-01 14:18:40 +0800

    1

---
+++ src/main/java/com/zy/txt/Text1.java
@@ -0,0 +1,11 @@
+package com.zy.txt;
+
+/**
+ * @author 这是那头猪
+ * @date 2022年05月01日 14:18
+ */
+public class Text1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
